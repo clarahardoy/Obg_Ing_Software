a7822f024902e01c520b790673d3b1f5b2ae22ae
VT341680@fi365.ort.edu.uy <VT341680@fi365.ort.edu.uy>
2025-07-31 19:09:04 -0300

    Secciones: reserva, barberos

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-/* ===== Navegación móvil ===== */
+/* Nav mobile */
 const btnMenu  = document.getElementById('btnMenu');
 const navLinks = document.getElementById('navLinks');
 
@@ -7,7 +7,6 @@ btnMenu.addEventListener('click', () => {
   btnMenu.classList.toggle('active');
 });
 
-/* Cierra el menú al hacer clic en un enlace */
 navLinks.querySelectorAll('a').forEach(link =>
   link.addEventListener('click', () => {
     if (navLinks.classList.contains('open')){
